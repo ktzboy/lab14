fac05007d8a5781c2fdf9826b4414263b4b1f98f
ktzboy <pongpol.sritalanut@gmail.com>
2025-01-24 17:02:36 +0700

    finish

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -21,3 +21,29 @@ int main(){
 }
 
 //Do not modify source code above this line 
+void inputMatrix(double matrix[][N]) {
+	for(int i=0 ; i<N ; i++) {
+		cout << "Row " << i+1 << ": ";
+		for(int j=0 ; j<N ; j++) {
+			cin >> matrix[i][j];
+		}
+	}
+}
+
+void findLocalMax(const double matrix[][N], bool matrixbool[][N]) {
+    for(int i=1 ; i<N-1 ; i++) {
+		for(int j=1 ; j<N-1 ; j++) {
+			if(matrix[i][j] >= matrix[i-1][j] and matrix[i][j] >= matrix[i+1][j] and matrix[i][j] >= matrix[i][j+1] and matrix[i][j] >= matrix[i][j-1]) matrixbool[i][j] = 1;
+			else matrixbool[i][j] = 0;
+		}
+	}
+}
+
+void showMatrix(const bool matrix[][N]) {
+    for(int i=0 ; i<N ; i++) {
+		for(int j=0 ; j<N ; j++) {
+			cout << matrix[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
